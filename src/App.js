47624710db98862470509f689380a9118c2ebd0e
fragata-adrian <fragataadrian99@gmail.com>
2020-11-20 20:44:48 -0300

    listo el form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // Creo un state para guardar los asociados
+
+  const [asociados, setAsociados] = useState([]);
+
+  // Funcion para tomar el socio nuevo y meterlo en el state de asociados
+
+  const addSocio = (socio) => {
+    setAsociados([
+      ...asociados,
+      socio
+    ]);
+  }
+
   return (
     <Fragment>
       <Container>
@@ -13,7 +27,9 @@ function App() {
         </Row>
         <Row>
           <Col>
-            <Formulario/>
+            <Formulario
+              addSocio={addSocio}
+            />
           </Col>
           <Col>Listado de Socios</Col>
         </Row>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,94 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Col, Alert } from 'react-bootstrap';
+import uuid from 'uuid/dist/v4';
 
 // slr
-const Formulario = () => {
+const Formulario = ({ addSocio }) => {
+
+    // Creo un socio vacio y lo inicializo con su state.
+
+    const [socio, setSocio] = useState({
+        firstname:"",
+        lastname:"",
+        dni:"",
+        email:"",
+        password:""
+    });
+
+    //Extraigo los valores del socio
+
+    const { firstname, lastname, dni, email, password } = socio;
+
+    // Creo otro state para el error del formulario
+
+    const [error, setError] = useState(false);
+
+    // Leer lo que se escribe.
+
+    const handleChange = (e) => {
+        setSocio({
+            ...socio,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    // Funcion para enviar el formulario
+    const submitForm = (e) => {
+        // No dejo que envie por HTML
+        e.preventDefault();
+        
+        // Validar los campos del formulario
+        if (firstname.trim() === "" || lastname.trim() === "" || dni.trim() === "" || email.trim() === "" || password.trim() === "") {
+            setError(true);
+            return
+        }
+
+        // Quitar el mensaje de error
+        setError(false);
+
+        // Poner un id al socio
+        socio.id = uuid();
+        console.log(socio);
+
+        // Enviar el objeto socio
+        addSocio(socio);
+
+        // Limpiar el formulario
+        setSocio({
+            firstname:"",
+            lastname:"",
+            dni:"",
+            email:"",
+            password:""
+        })
+    }
+
     return (
         <Fragment>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Nombre de Usuario</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="Ingrese su apellido y nombre sin espacios" 
-                        name="user"
-                    />
-                    <Form.Text className="text-muted">
-                        Puede utilizar mayúsculas o un "-" para separar.
-                    </Form.Text>
-                </Form.Group>
+            <Form onSubmit={submitForm} >
+                <Form.Row>
+                    <Col>
+                        <Form.Label>Nombre</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Nombre" 
+                            name="firstname"
+                            onChange={handleChange}
+                            value={firstname}
+                        />
+                    </Col>
+                    <Col>
+                        <Form.Label>Apellido</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            placeholder="Apellido" 
+                            name="lastname"
+                            onChange={handleChange}
+                            value={lastname}
+                        />
+                    </Col>
+                </Form.Row>
 
                 <Form.Group>
                     <Form.Label>DNI</Form.Label>
@@ -25,6 +96,8 @@ const Formulario = () => {
                         type="text" 
                         placeholder="Sin puntos ni espacios"
                         name="dni"
+                        onChange={handleChange}
+                        value={dni}
                     />
                 </Form.Group>
 
@@ -34,6 +107,8 @@ const Formulario = () => {
                         type="email" 
                         placeholder="Ingrese su email" 
                         name="email"
+                        onChange={handleChange}
+                        value={email}
                     />
                 </Form.Group>
 
@@ -43,12 +118,22 @@ const Formulario = () => {
                         type="password" 
                         placeholder="Utilice números y letras"
                         name="password"
+                        onChange={handleChange}
+                        value={password}
                         />
                 </Form.Group>
                 
-                <Button variant="primary" type="submit">
+                <Button 
+                    variant="primary" 
+                    type="submit"
+                >
                     Registrarse
                 </Button>
+
+                {error
+                    ? <Alert variant="danger">Debes completar todos los campos</Alert>
+                    : null
+                }
             </Form>
         </Fragment>
     );
